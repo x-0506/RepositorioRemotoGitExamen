b7fddb6d8ac3c580f86549a204d42097648c3274
x <ad2738992@gmail.com>
2024-05-16 18:29:38 +0200

    "Segunda rama"

--- ProyectoGitExamen/src/paqueteGitExamen/GitExamen.java
+++ ProyectoGitExamen/src/paqueteGitExamen/GitExamen.java
@@ -3,7 +3,7 @@ package paqueteGitExamen;
 public class GitExamen {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Esto es un commit");
 
 	}
 
